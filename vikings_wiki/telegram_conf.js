b6d74bc3a58333bacb55674e496d974ba70bbf21
PavelB <pavelb@ifn-solutions.com>
2018-11-05 18:12:19 +0200

    Version 1.0 - Vikings bot

--- vikings_wiki/telegram_conf.js
+++ vikings_wiki/telegram_conf.js
@@ -1,7 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '183719913:AAE8ST-eVf0Afzu3JILGlzq8GuaVK6hGfHE';
 const bot = new TelegramBot(token, { polling: true });
-const localDb = require('../DB/local_db');
 var fs = require('fs');
 var config = JSON.parse(fs.readFileSync('./vikings_wiki/config.json', 'utf8'));
 
@@ -13,14 +12,6 @@ bot.on('message', function (msg, match) {
 function checkBotEvent(msg, fromId) {
     var isCommand = checkCommandEvent(msg, fromId);
     if (isCommand) return true;
-
-
-    if (msg.text && msg.text.includes("@PauloBotDevbot")) {//TODO: Turn on speaker bot
-        var enter = msg.text;
-        enter = enter.substr(16,msg.text.length);
-        var isSpeaking = checkSpeakingEvent(enter, fromId); 
-        if (isSpeaking) return true;
-    }
     return false;
 }
 
@@ -39,34 +30,20 @@ function checkCommandEvent(msg, fromId) {
         console.log("Request: " + enter);
         if (enter == '/id') {
             bot.sendMessage(fromId, msg.from.id);
-        }
-        else if (enter == '/help' || enter == '/помощь' || enter == '/start') {
+        } else if (enter == '/help' || enter == '/помощь' || enter == '/start') {
             bot.sendMessage(fromId, config.help);
-        }
-        else if (enter == '/helpall' || enter == '/all') {
+        } else if (enter == '/helpall' || enter == '/all') {
             bot.sendMessage(fromId, config.all);
-        }
-        else{
-            fs.appendFile('viking_wiki_bot.log', enter+"\n", function (err) {
+        } else {
+            fs.appendFile('viking_wiki_bot.log', enter + "\n", function (err) {
                 if (err) throw err;
-              });
+            });
         }
         return true;
     }
     return false;
 }
 
-function checkSpeakingEvent(msg, fromId) {
-    var message = msg.toLowerCase();
-    var ans = localDb.getAnswer(message);
-    if (ans) {
-        bot.sendMessage(fromId, ans);
-        return true;
-    } else {
-       // bot.sendMessage(fromId, "Попробуй еще раз");
-        return false;
-    }
-}
 
 
 function chatOrUserController(msg) {
